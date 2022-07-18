b7dec37e4d58c22daa7db72fc461f521f3e08110
siyeooon <psy001225@naver.com>
2022-07-18 23:03:03 +0900

    fast

---
+++ src/routes/calcurator/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Button } from '@src/components/button';
+
+	const textNumbers: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+</script>
+
+<div class="frame">
+	{#each textNumbers as text}
+		<Button {text} color="orange" onClick={(value) => alert(value)} />
+	{/each}
+</div>
+
+<style lang="scss">
+	.frame {
+		width: 240px;
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
